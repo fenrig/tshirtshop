f015c2804beaef51a9aa4ad608bf1599db938734
Matthias Van Gestel <maregt0@gmail.com>
2013-04-22 17:03:52 +0200

    Extend tshirts view

--- view/global/main_view_tshirts.php
+++ view/global/main_view_tshirts.php
@@ -11,6 +11,15 @@
 				<img class="fullnail" src="<?php echo 'http://' . $_SERVER['SERVER_ADDR'] . ':8081/thumbn/' . $tshirt['cid']; ?>" 
 				onmouseover="this.src='<?php echo 'http://' . $_SERVER['SERVER_ADDR'] . ':8081/fulln/' . $tshirt['cid']; ?>'" 
 				onmouseout="this.src='<?php echo 'http://' . $_SERVER['SERVER_ADDR'] . ':8081/thumbn/' . $tshirt['cid']; ?>'"/>
+				<span class="price">
+					<?php
+					$number = number_format($tshirt['price'], 2, '.', '');
+					$price = explode('.', $number);
+					?>
+					<span class="u">&#8364;</span>
+					<span class="d"><?php echo $price[0] ?></span>
+					<span class="c"><?php echo '.' . $price[1] ?></span>
+				</span>
 			</a>
 		<?php
 	}
